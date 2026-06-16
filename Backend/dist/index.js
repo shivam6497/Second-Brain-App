18c3fd1a4b93744913f2e7633cca1a88ea02859e
shivam6497 <kdmboy6397@gmail.com>
2026-06-16 14:28:57 +0000

    Initial Commit

---
+++ Backend/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 8;
+console.log(num);
+//# sourceMappingURL=index.js.map
---
+++ Backend/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import zod  from 'zod';
+import jwt from 'jsonwebtoken';
+
+
+const app = express();
+
+app.post()
